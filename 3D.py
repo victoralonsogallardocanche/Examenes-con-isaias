28b53db6b0d6503ff95971f12951684c3e59fd60
victoralonsogallardocanche <67614330+victoralonsogallardocanche@users.noreply.github.com>
2021-02-01 13:50:58 -0500

    Examen 3d

---
+++ 3D.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import math as mt
+
+
+print("Por favor proporcione las cordenadas del hit point")
+print("Comencemos con la x")
+x=input()
+print("ahora la y")
+y=input()
+
+#creacion de el Lienzo
+plt.axis([-20, 20, -20, 20])
+
+
+
+
+plt.show()
